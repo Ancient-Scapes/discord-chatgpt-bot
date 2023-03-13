ee6edca362e08cdfe90684f36ab5a5398bce10d4
Ancient-Scapes <beatmaniaiidx.resortanthem@gmail.com>
2023-03-13 23:44:24 +0900

    横に120文字書いてもOK

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,6 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
-    "max-len": 1,
+    "max-len": ["error", {"code": 120}],
   },
 };
